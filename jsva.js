f69738185b00aa211bbce7d88422bb259455aedf
NAzarovKR <58940336+NAzarovKR@users.noreply.github.com>
2019-12-17 13:27:25 +0300

    крутая идея

---
+++ jsva.js
@@ -0,0 +1,13 @@
+let a = document.querySelector('.p1');
+let b = document.querySelector('.b1');
+let nemberCalculator = function (c,y){
+return c*y
+}
+
+
+b.addEventListener('click',function(){
+a.style.color="blue"
+});
+b.addEventListener('click',function(){
+  a.style.color='red'
+})
